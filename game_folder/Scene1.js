ae9012f1ae08524d2c3448722cdb75d712822b99
Squidknee <72271842+Squidknee@users.noreply.github.com>
2024-07-29 18:42:10 -0400

    spritesheets added and loaded in

--- game_folder/Scene1.js
+++ game_folder/Scene1.js
@@ -5,13 +5,21 @@ class Scene1 extends Phaser.Scene {
   
     preload()
     {
-        
+        this.load.spritesheet("char1", "./assets/characters/char1.png", {frameHeight: 32, frameWidth: 32});
+        this.load.spritesheet("char2", "./assets/characters/char2.png", {frameHeight: 32, frameWidth: 32});
+        this.load.spritesheet("char3", "./assets/characters/char3.png", {frameHeight: 32, frameWidth: 32});
+        this.load.spritesheet("char4", "./assets/characters/char4.png", {frameHeight: 32, frameWidth: 32});
+        this.load.spritesheet("char5", "./assets/characters/char5.png", {frameHeight: 32, frameWidth: 32});
+        this.load.spritesheet("char6", "./assets/characters/char6.png", {frameHeight: 32, frameWidth: 32});
+        this.load.spritesheet("char7", "./assets/characters/char7.png", {frameHeight: 32, frameWidth: 32});
+        this.load.spritesheet("char8", "./assets/characters/char8.png", {frameHeight: 32, frameWidth: 32});
     }
   
     create() 
     {
         this.add.text(20, 20, "Loading game...");
         this.scene.start("playGame");
+        this.add.image(40,40, "char1")
         
     }
 }
