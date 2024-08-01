b8c16587d1f63974f72d6640261d776a4fdaf31b
Squidknee <72271842+Squidknee@users.noreply.github.com>
2024-07-31 22:12:05 -0400

    animated players

--- game_folder/GameScene.js
+++ game_folder/GameScene.js
@@ -24,10 +24,19 @@ class GameScene extends Phaser.Scene {
 
         // Function to get a random position within the game area
         const getRandomPosition = () => {
-            const x = Phaser.Math.Between(50, this.sys.game.config.width - 50);
-            const y = Phaser.Math.Between(50, this.sys.game.config.height - 50);
+            const x = Phaser.Math.Between(10, this.sys.game.config.width - 10);
+            const y = Phaser.Math.Between(100, this.sys.game.config.height - 10);
             return { x, y };
         };
+
+        textures.forEach(texture => {
+            this.anims.create({
+                key: `${texture}_hover`,
+                frames: this.anims.generateFrameNumbers(texture, { start: 0, end: 1 }),
+                frameRate: 5,
+                repeat: -1
+            });
+        });
         
         // Sets the texture that will be used for the wanted person
         // Will be checked against all the other characters to make sure it's not used twice
@@ -48,6 +57,14 @@ class GameScene extends Phaser.Scene {
             }
             
             const character = new Character(this, position.x, position.y, texture, 0);
+            character.setInteractive();
+            character.on('pointerover', () => {
+                character.play(`${texture}_hover`);
+            });
+            character.on('pointerout', () => {
+                character.stop(); // Stops the hover animation
+                character.setFrame(0); // Reset to default frame
+            });
             this.add.existing(character);
             this.objects.push(character);
         }
@@ -55,9 +72,16 @@ class GameScene extends Phaser.Scene {
         // Creates final character (the correct one) at a random position
         let position = getRandomPosition();
         const character = new Character(this, position.x, position.y, correctTexture, 1);
+        character.setInteractive();
+        character.on('pointerover', () => {
+            character.play(`${correctTexture}_hover`);
+        });
+        character.on('pointerout', () => {
+            character.stop(); // Stops the hover animation
+            character.setFrame(0); // Reset to default frame
+        });
         this.add.existing(character);
         this.objects.push(character);
-        this.error = this.sound.add("error");
         
         // Place timer on screen
         /**this.timerText = this.add.text((this.sys.game.config.width - 100), (this.sys.game.config.height - 50), 'Time: ' + initialTime, {
